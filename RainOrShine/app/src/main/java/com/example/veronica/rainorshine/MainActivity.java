ddf455f0c921b08f384f38dea7f3a38994b10502
Veronica <veronicawu95@hotmail.com>
2016-10-15 22:50:53 -0700

    Adding a toast

--- RainOrShine/app/src/main/java/com/example/veronica/rainorshine/MainActivity.java
+++ RainOrShine/app/src/main/java/com/example/veronica/rainorshine/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("CAN YOU SEE THIS", "THIS IS AN EDIT");
         Log.d("ANOTHER", "TEST");
+        Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_SHORT).show();
     }
 }
