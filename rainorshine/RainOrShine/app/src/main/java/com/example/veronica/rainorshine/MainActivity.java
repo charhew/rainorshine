3c86ca38cdd04401da5674649dcc0b247d850781
Veronica <veronicawu95@hotmail.com>
2016-10-15 22:21:28 -0700

    Fixing stuff

--- rainorshine/RainOrShine/app/src/main/java/com/example/veronica/rainorshine/MainActivity.java
+++ rainorshine/RainOrShine/app/src/main/java/com/example/veronica/rainorshine/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.veronica.rainorshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("CAN YOU SEE THIS", "THIS IS AN EDIT");
     }
 }
