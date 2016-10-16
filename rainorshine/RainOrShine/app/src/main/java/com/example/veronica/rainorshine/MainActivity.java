d475dcf7069c4bfa4fef8e7077cf11b6b140dbe0
Veronica <veronicawu95@hotmail.com>
2016-10-15 22:32:03 -0700

    Adding gitignore file

--- rainorshine/RainOrShine/app/src/main/java/com/example/veronica/rainorshine/MainActivity.java
+++ rainorshine/RainOrShine/app/src/main/java/com/example/veronica/rainorshine/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.veronica.rainorshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("CAN YOU SEE THIS", "THIS IS AN EDIT");
+        Log.d("ANOTHER", "TEST");
     }
 }
