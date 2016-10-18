38e2d22b2352a6ee5d11bb070e50749f2d66dd90
Veronica <veronicawu95@hotmail.com>
2016-10-18 00:18:32 -0700

    Adds functionality so upon successful registration, login activity starts

--- RainOrShine/app/src/main/java/com/example/veronica/rainorshine/Register.java
+++ RainOrShine/app/src/main/java/com/example/veronica/rainorshine/Register.java
@@ -1,5 +1,6 @@
 package com.example.veronica.rainorshine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -41,6 +42,7 @@ public class Register extends AppCompatActivity {
                 c.setPassword(pass1);
 
                 helper.insertContact(c);
+                startActivity(new Intent(this, Login.class));
             }
         }
     }
